ddb77f64eee850865117d09b6f8e4c313be21fad
Jake Harding <jacob.s.harding@gmail.com>
2012-07-14 00:46:22 -0700

    Add initial draft of photographer.js.

---
+++ photographer.js
@@ -0,0 +1,121 @@
+/*globals */
+
+(function() {
+
+  // default configurations
+  var defaults = {
+    debug: false,
+    container: null
+  };
+
+  // Photographer
+  // -----------
+
+  var Photographer = function(config) {
+    config = extend(defaults, config);
+
+    this._stream = null;
+
+    this._photos = [];
+    this._latestPhoto = null;
+
+    this._container = config.container;
+
+    // dimensions of container element
+    var width = this._container.clientWidth;
+    var height = this._container.clientHeight;
+
+    // video element is where the webcam stream
+    // gets piped to for the live preview
+    this._video = document.createElement('video');
+    this._video.width = width;
+    this._video.height = height;
+    this._video.autoplay = true;
+    this._container.appendChild(this._video);
+
+    // canvas element is used to capture images
+    this._canvas = document.createElement('canvas');
+    this._canvas.width = width;
+    this._canvas.height = height;
+
+    this._context = this._canvas.getContext('2d');
+  };
+
+  Photographer.prototype.start = function() {
+    var that = this;
+
+    // getUserMedia worked :)
+    var pipeStreamToVideo = function(stream) {
+      that._stream = stream;
+
+      window.URL ? that._video.src = window.URL.createObjectURL(stream) :
+                   that._video.src = stream;
+    };
+
+    // getUserMedia failed :(
+    var handleError = function(error) {
+      console.error(error);
+    };
+
+    navigator.getUserMedia({ video: true }, pipeStreamToVideo, handleError);
+  };
+
+  Photographer.prototype.stop = function() {
+    this._stream && this._stream.stop();
+
+    delete this._stream;
+  };
+
+  Photographer.prototype.takePhoto = function() {
+    this._context.drawImage(this._video, 0, 0);
+
+    this._latestPhoto = this._canvas.toDataURL('image/png');
+    this._photos.push(this._latestPhoto);
+  };
+
+  Photographer.prototype.getPhotos = function() {
+    // return a copy of the photos array
+    return this._photos.slice(0);
+  };
+
+  Photographer.prototype.getLatestPhoto = function() {
+    return this._latestPhoto;
+  };
+
+  // expose globally
+  window.Photographer = Photographer;
+
+  // normalization
+  // -------------
+
+  window.URL = window.URL || window.webkitURL || window.mozURL ||
+               window.msURL || window.oURL;
+
+  navigator.getUserMedia = navigator.getUserMedia ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia ||
+                           navigator.oGetUserMedia;
+
+  // helper functions
+  // ----------------
+
+  var extend = function(target, obj) {
+    var extendedObj = {};
+
+    for (var key in target) {
+      if (target.hasOwnProperty(key)) {
+        extendedObj[key] = target[key];
+      }
+    }
+
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        extendedObj[key] = obj[key];
+      }
+    }
+
+    return extendedObj;
+  };
+
+})();
