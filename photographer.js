9cec89c124b59b1767d6b8864284ff206c1c91a1
Jake Harding <jacob.s.harding@gmail.com>
2012-07-18 09:34:41 -0700

    Provide ability to set dimensions of taken photos.

--- photographer.js
+++ photographer.js
@@ -20,7 +20,9 @@
   // default configurations
   var defaults = {
     flash: null,
-    container: null
+    container: null,
+    imgWidth: null,
+    imgHeight: null
   };
 
   var Photographer = function(config) {
@@ -37,6 +39,11 @@
     var width = this._container.clientWidth;
     var height = this._container.clientHeight;
 
+    // if imgWidth and imgHeight weren't explicitly set,
+    // inherit values from the dimensions of the container
+    this._config.imgWidth || (this._config.imgWidth = width);
+    this._config.imgHeight || (this._config.imgHeight = height);
+
     // video element is where the webcam stream
     // gets piped to for the live preview
     this._video = document.createElement('video');
@@ -47,8 +54,8 @@
 
     // canvas element is used to capture images
     this._canvas = document.createElement('canvas');
-    this._canvas.width = width;
-    this._canvas.height = height;
+    this._canvas.width = this._config.imgWidth;
+    this._canvas.height = this._config.imgHeight;
 
     this._context = this._canvas.getContext('2d');
   };
@@ -82,7 +89,8 @@
     // if flash function is present, call it
     this._config.flash && this._config.flash(this._container);
 
-    this._context.drawImage(this._video, 0, 0);
+    this._context.drawImage(this._video, 0, 0, this._config.imgWidth,
+                            this._config.imgHeight);
 
     this._latestPhoto = this._canvas.toDataURL('image/png');
     this._photos.push(this._latestPhoto);
